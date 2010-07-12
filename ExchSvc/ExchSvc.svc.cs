03099cdbc99900013798c2467acb1409e995ff18
unknown <Administrator@.thetoolbox.com>
2010-07-12 15:53:48 -0500

    Commiting latest code changes.  Added method ChangePassword, added documentation.

--- ExchSvc/ExchSvc.svc.cs
+++ ExchSvc/ExchSvc.svc.cs
@@ -35,88 +35,104 @@ namespace TALHO
     // NOTE: If the service is renamed, remember to update the global.asax.cs file
     public class ExchSvc
     {
+        // GetCollection()
+        // desc: returns paginated list of current exchange users
+        // params: int current_page - Page to return, defaults to 1 
+        //         int per_page     - Results per page to return, defaults to 10
+        // method: Web Get
+        // return: ExchangeUsers Object
         [WebGet(UriTemplate = "?page={current_page}&per_page={per_page}")]
         public ExchangeUsers GetCollection(int current_page, int per_page)
         {
-            current_page = current_page < 1 ? 1 : current_page;
-            per_page = per_page < 1 ? 10 : per_page;
-            string result = ExchangeUser.GetUser("",current_page, per_page);
-            string[] seperator = new string[1];
-            seperator[0] = "THEWORLDSLARGESTSEPERATOR";
-            string[] results = result.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
-            result = results[0];
-            XmlSerializer serializer = new XmlSerializer(typeof(ExchangeUserCollection));
-            StringReader textReader = new StringReader(result);
+            current_page                 = current_page < 1 ? 1 : current_page;
+            per_page                     = per_page < 1 ? 10 : per_page;
+            string result                = ExchangeUser.GetUser("",current_page, per_page);
+            string[] seperator           = new string[1];
+            seperator[0]                 = "THEWORLDSLARGESTSEPERATOR";
+            string[] results             = result.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+            result                       = results[0];
+            XmlSerializer serializer     = new XmlSerializer(typeof(ExchangeUserCollection));
+            StringReader textReader      = new StringReader(result);
             ExchangeUserCollection users = (ExchangeUserCollection)serializer.Deserialize(textReader);
-            textReader.Close();
-            users.current_page = current_page < 1 ? 1 : current_page; 
-            users.per_page = per_page < 1 ? 10 : per_page;
-            users.total_entries = Int32.Parse(results[1]);
+            users.current_page           = current_page < 1 ? 1 : current_page;
+            users.per_page               = per_page < 1 ? 10 : per_page;
+            users.total_entries          = Int32.Parse(results[1]);
+            textReader.Close(); 
             return users.toXML();
         }
 
+        // Create()
+        // desc: Creates user in exchange.  If isVPN flag is set, create user in AD only under OU=VPN,OU=TALHO and with "-vpn" appended to their alias/identity
+        // params: XML post from the client server
+        // method: Web Post HTTP/XML
+        // return: Serialized XML representation of ExchangeUser object
         [OperationContract]
         [WebInvoke(UriTemplate = "/", Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml)]
         public ExchangeUser Create()
         {
-
-            StringWriter sw   = new StringWriter();
-            XmlTextWriter xtw = new XmlTextWriter(sw);
+            //begin read in xml
+            StringWriter sw     = new StringWriter();
+            XmlTextWriter xtw   = new XmlTextWriter(sw);
+            string activeResult = "";
             OperationContext.Current.RequestContext.RequestMessage.WriteMessage(xtw);
             xtw.Flush();
             xtw.Close(); 
             XmlTextReader xtr                     = new XmlTextReader(new StringReader(sw.ToString()));
             string s                              = xtr.ReadElementString("Binary");
             string s1                             = System.Text.ASCIIEncoding.ASCII.GetString(System.Convert.FromBase64String(s));
-            Dictionary<string, string> attributes = new Dictionary<string,string>();              
+            Dictionary<string, string> attributes = new Dictionary<string, string>();              
             System.Xml.XmlDocument xml_doc        = new System.Xml.XmlDocument();
             xml_doc.LoadXml(@s1);
 
-            attributes.Add("alias",  xml_doc.SelectSingleNode("/ExchSvc/alias").InnerText);
-            attributes.Add("domain", xml_doc.SelectSingleNode("/ExchSvc/domain").InnerText);
-            attributes.Add("upn", xml_doc.SelectSingleNode("/ExchSvc/userPrincipalName").InnerText);
-            attributes.Add("cn", xml_doc.SelectSingleNode("/ExchSvc/cn").InnerText);
-            attributes.Add("name", xml_doc.SelectSingleNode("/ExchSvc/name").InnerText);
-            attributes.Add("displayName", xml_doc.SelectSingleNode("/ExchSvc/displayName").InnerText);
-            attributes.Add("dn" , xml_doc.SelectSingleNode("/ExchSvc/distinguishedName").InnerText);
-            attributes.Add("givenName", xml_doc.SelectSingleNode("/ExchSvc/givenName").InnerText);
-            attributes.Add("samAccountName", xml_doc.SelectSingleNode("/ExchSvc/samAccountName").InnerText);
-            attributes.Add("unicodePwd", xml_doc.SelectSingleNode("/ExchSvc/unicodePwd").InnerText);
-            attributes.Add("sn", xml_doc.SelectSingleNode("/ExchSvc/sn").InnerText);
-            attributes.Add("changePwd", xml_doc.SelectSingleNode("/ExchSvc/changePwd").InnerText);
-            attributes.Add("isVPN", xml_doc.SelectSingleNode("/ExchSvc/isVPN").InnerText);
-            attributes.Add("acctDisabled", xml_doc.SelectSingleNode("/ExchSvc/acctDisabled").InnerText);
-            attributes.Add("pwdExpires", xml_doc.SelectSingleNode("/ExchSvc/pwdExpires").InnerText);
-            attributes.Add("ou", xml_doc.SelectSingleNode("/ExchSvc/ou").InnerText);
+            //read in attributes from xml_doc, store them in Dictionary variable attributes
+            attributes.Add("alias", xml_doc.SelectSingleNode("/ExchSvc/alias") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/alias").InnerText);
+            attributes.Add("domain", xml_doc.SelectSingleNode("/ExchSvc/domain") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/domain").InnerText);
+            attributes.Add("upn", xml_doc.SelectSingleNode("/ExchSvc/userPrincipalName") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/userPrincipalName").InnerText);
+            attributes.Add("cn", xml_doc.SelectSingleNode("/ExchSvc/cn") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/cn").InnerText);
+            attributes.Add("name", xml_doc.SelectSingleNode("/ExchSvc/name") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/name").InnerText);
+            attributes.Add("displayName", xml_doc.SelectSingleNode("/ExchSvc/displayName") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/displayName").InnerText);
+            attributes.Add("dn" , xml_doc.SelectSingleNode("/ExchSvc/distinguishedName") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/distinguishedName").InnerText);
+            attributes.Add("givenName", xml_doc.SelectSingleNode("/ExchSvc/givenName") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/givenName").InnerText);
+            attributes.Add("samAccountName", xml_doc.SelectSingleNode("/ExchSvc/samAccountName") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/samAccountName").InnerText);
+            attributes.Add("password", xml_doc.SelectSingleNode("/ExchSvc/password") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/password").InnerText);
+            attributes.Add("sn", xml_doc.SelectSingleNode("/ExchSvc/sn") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/sn").InnerText);
+            attributes.Add("changePwd", xml_doc.SelectSingleNode("/ExchSvc/changePwd") == null ? "0" : xml_doc.SelectSingleNode("/ExchSvc/changePwd").InnerText);
+            attributes.Add("isVPN", xml_doc.SelectSingleNode("/ExchSvc/isVPN") == null ? "0" : xml_doc.SelectSingleNode("/ExchSvc/isVPN").InnerText);
+            attributes.Add("acctDisabled", xml_doc.SelectSingleNode("/ExchSvc/acctDisabled") == null ? "0" : xml_doc.SelectSingleNode("/ExchSvc/acctDisabled").InnerText);
+            attributes.Add("pwdExpires", xml_doc.SelectSingleNode("/ExchSvc/pwdExpires") == null ? "1" : xml_doc.SelectSingleNode("/ExchSvc/pwdExpires").InnerText);
+            attributes.Add("ou", xml_doc.SelectSingleNode("/ExchSvc/ou") == null ? "" : xml_doc.SelectSingleNode("/ExchSvc/ou").InnerText);
 
-            string newResult = ExchangeUser.NewExchangeUser(attributes);
-            
-            if (newResult.IndexOf("Error") != -1)
+            //Call NewExchangeUser on ExchangeUser class
+            string exchangeResult = ExchangeUser.NewExchangeUser(attributes);
+
+            if (exchangeResult.IndexOf("Error") != -1)
             {
                 WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
             }
-            //ExchangeUser userResult = Get(attributes["alias"]);
             if (attributes["isVPN"] == "1")
             {
                 attributes["alias"]          = attributes["alias"] + "-vpn";
                 attributes["upn"]            = attributes["upn"].Replace("@", "-vpn@");
-                attributes["dn"]             += "OU=VPN";
+                attributes["dn"]             = attributes["dn"].Replace("OU=TALHO", "OU=VPN,OU=TALHO");
                 attributes["samAccountName"] = attributes["samAccountName"] + "-vpn";
-                ExchangeUser.NewADUser(attributes);
+                activeResult                 = ExchangeUser.NewADUser(attributes);
+                if (activeResult.IndexOf("Error") != -1)
+                {
+                    WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
+                }
             }
-            //string exchResult        = ExchangeUser.EnableMailbox(attributes["upn"], attributes["alias"]).ToString();
             XmlSerializer serializer = new XmlSerializer(typeof(ExchangeUser));
-            StringReader textReader = new StringReader(newResult);
+            StringReader textReader  = new StringReader(exchangeResult);
             ExchangeUser user        = (ExchangeUser)serializer.Deserialize(textReader);
-            //user.upn                 = attributes["upn"];
-           // user.alias               = attributes["alias"];
             textReader.Close();
-            
             return user;
-
         }
 
-
+        // Get()
+        // desc: Find and return ExchangeUser
+        // params: string alias - User login name
+        // method: Web Get
+        // return: Serialized XML representation of ExchangeUser object
         [WebGet(UriTemplate = "{alias}")]
         public ExchangeUser Get(string alias)
         {
@@ -133,12 +149,22 @@ namespace TALHO
             return user;
         }
 
-        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
-        public void Update(string id)
+        // Update()
+        // desc: Method not implemented, returns a "500" code for not implemented
+        // params: string alias - User login name
+        // method: Web Put
+        // return: void
+        [WebInvoke(UriTemplate = "{alias}", Method = "PUT")]
+        public void Update(string alias)
         {
             WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotImplemented;
         }
 
+        // Delete()
+        // desc: Method deletes user from Exchange
+        // params: string alias - User login name
+        // method: Web Post HTTP/XML
+        // return: void, method sets WebOperationContext status code to either OK on success or Not Found on failure
         [OperationContract]
         [WebInvoke(UriTemplate = "{alias}/delete", Method = "POST")]
         public void Delete(string alias)
@@ -157,6 +183,35 @@ namespace TALHO
                     WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
             }
         }
-        
+
+        // ChangePassword()
+        // desc: Method resets user password with new password given
+        // params: string alias    - User login name
+        //         string password - New Password, plain text
+        // method: Web Post HTTP/XML
+        // return: void, method sets WebOperationContext status code to OK or Not Found
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{alias}/update?password={password}", Method = "POST", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Xml)]
+        public void ChangePassword(string alias, string password)
+        {
+            Dictionary<string, string> attributes = new Dictionary<string, string>();
+  
+            attributes.Add("identity", alias == null ? "" : alias);
+            attributes.Add("password", password == null ? "" : password);
+
+            ExchangeUser result = Get(alias);
+            if (result.upn.CompareTo("") == 0)
+            {
+                WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
+            }
+            else
+            {
+                bool r = ExchangeUser.ChangePassword(attributes);
+                if (r)
+                    WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                else
+                    WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
+            }
+        }   
     }
 }
--- ExchSvc/ExchangeUser.cs
+++ ExchSvc/ExchangeUser.cs
@@ -9,11 +9,14 @@ using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
+// Scope TALHO
 namespace TALHO
 {
+    // Class ExchangeUser
     [DataContract(Name="exchange-user")]
     public class ExchangeUser
     {
+        // Declare public fields
         [DataMember(Name = "alias")]
         public string alias { get; set; }
         [DataMember(Name = "dn")]
@@ -32,72 +35,118 @@ namespace TALHO
         public string email { get; set; }
         [DataMember(Name = "has_vpn")]
         public bool has_vpn { get; set; }
+        [DataMember(Name = "error")]
+        public string error { get; set; }
 
-
+        // GetUser()
+        // desc: Method calls PowerShellComponent command GetUser, returns list of user/users
+        // params: string thealias  - User login name, passed in by ExchSvc
+        //         int current_page - Page to return
+        //         int per_page     - Number of entries to return per page
+        // method: public 
+        // return: string, XML string of ExchangeUser object
         public static string GetUser(string thealias, int current_page, int per_page)
         {
-            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
-
             string result;
-            result = objManage.GetUser(thealias, current_page, per_page);
-
-            objManage = null;
+            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
+            result                                           = objManage.GetUser(thealias, current_page, per_page);
+            objManage                                        = null;
             return result;
         }
 
+        // NewExchangeUser()
+        // desc: Method calls PowerShellComponent command NewExchangeUser, creates new mailbox user on Exchange Server
+        // params: Dictionary<string, string> attributes - Dictionary object, contains attributes for new user
+        // method: public
+        // return: string, XML string of ExchangeUser object
         public static string NewExchangeUser(Dictionary<string, string> attributes)
         {
-            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
-
-            string Results;
-            Results = objManage.NewExchangeUser(attributes);
-
+            PowerShellComponent.ManagementCommands objManage = null;
+            string Results                                   = null;
+            try{
+                objManage = new PowerShellComponent.ManagementCommands();
+                Results   = objManage.NewExchangeUser(attributes);
+            }catch (Exception e){
+                return e.Message;
+            }
             objManage = null;
             return Results;
         }
 
+        // EnableMailbox()
+        // desc: Method calls PowerShellComponent command EnableMailbox, enables user mailbox on Exchange Server
+        // params: string upn      - User Principal Name
+        //         string thealias - User login name
+        // method: public
+        // return: string, XML string of ExchangeUser object
         public static string EnableMailbox(string upn, string thealias)
         {
-
-            // Create the COM+ component
-            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
-
             string Results;
-            Results = objManage.EnableMailbox(upn, thealias);
-
-            objManage = null;
+            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
+            Results                                          = objManage.EnableMailbox(upn, thealias);
+            objManage                                        = null;
             return Results;
         }
 
+        // NewADUser()
+        // desc: Method calls PowerShellComponent command NewADUser, creates a user in ActiveDirectory only
+        // params: Dictionary<string, string> attributes - Dictionary object, contains attributes for new user
+        // method: public
+        // return: string, XML string of ExchangeUser object
         public static string NewADUser(Dictionary<string, string> attributes)
         {
-            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
-
-            string Results;
-            Results = objManage.NewADUser(attributes);
-
+            PowerShellComponent.ManagementCommands objManage = null;
+            string Results                                   = null;
+            try{
+                objManage = new PowerShellComponent.ManagementCommands();
+                Results   = objManage.NewADUser(attributes);
+            }catch (Exception e){
+                return e.Message;
+            }
             objManage = null;
             return Results;
         }
 
+        // ChangePassword()
+        // desc: Method calls PowerShellComponent command ChangePassword, changes user password with password given
+        // params: Dictionary<string, string> attributes - Dictionary object, contains attributes to change user password
+        // method: public
+        // return: bool
+        public static bool ChangePassword(Dictionary<string, string> attributes)
+        {
+            bool Results;
+            PowerShellComponent.ManagementCommands objManange = new PowerShellComponent.ManagementCommands();
+            Results                                           = objManange.ChangePassword(attributes);
+            objManange                                        = null;
+            return Results;
+        }
+
+        // RemoveMailBox()
+        // desc: Method calls PowerShellComponent command DeleteUser, deletes user from Exchange and AD Server
+        // params: string identity - User login name
+        // method: public
+        // return: bool
         public static bool RemoveMailbox(string identity)
         {
-            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
             bool Results;
-            Results = objManage.DeleteUser(identity);
-            objManage = null;
+            PowerShellComponent.ManagementCommands objManage = new PowerShellComponent.ManagementCommands();
+            Results                                          = objManage.DeleteUser(identity);
+            objManage                                        = null;
             return Results;
-        }
-
-        
+        }  
     }
 
+    // Class ExchangeUserCollection, can take in a list of ExchangeUser, serialize them, then return an XML with a list of ExchangeUsers
     public class ExchangeUserCollection : List<ExchangeUser>
     {
         public int current_page { get; set; }
         public int per_page { get; set; }
         public int total_entries { get; set; }
 
+        // toXML()
+        // desc: Method serializes a list of ExchangeUsers, returns serialized XML
+        // params: none
+        // return: ExchangeUsers, IXmlSerialized
         public ExchangeUsers toXML()
         {
             DataContractSerializer serializer = new DataContractSerializer(typeof(ExchangeUser));
@@ -118,6 +167,7 @@ namespace TALHO
         }
     }
 
+    // Class ExchangeUsers, takes in an XML object of ExchangeUser
     [XmlRoot(ElementName = "exchange-users")]
     public class ExchangeUsers : IXmlSerializable
     {
@@ -125,15 +175,24 @@ namespace TALHO
         private int current_page;
         private int per_page;
 
+        // ExchangeUsers()
+        // desc: Constructor, set Content, size, current_page, and per_page
+        // params: string content   - XML string 
+        //         int size         - total number of entries
+        //         int current_page - page to return
+        //         int per_page     - number of entries to return per page
+        // return: none
         public ExchangeUsers(string content, int size, int current_page, int per_page)
         {
-            this.Content = content;
-            this.size = size;
+            this.Content      = content;
+            this.size         = size;
             this.current_page = current_page;
-            this.per_page = per_page;
+            this.per_page     = per_page;
         }
+
         public string Content { get; set; }
 
+        //Overload IXmlSerializable
         #region IXmlSerializable Members
 
         public System.Xml.Schema.XmlSchema GetSchema()
